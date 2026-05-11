12b74dbd26213e3287a99fdfdc5aeb1853fa347c
Achim Friedland <achim.friedland@graphdefined.com>
2026-05-11 19:14:02 +0200

    Minor refactorings

--- OCPP_ChargingStation/Program.cs
+++ OCPP_ChargingStation/Program.cs
@@ -5,6 +5,7 @@ using OCPPv1_6 = cloud.charging.open.protocols.OCPPv1_6;
 using org.GraphDefined.Vanaheimr.Hermod.HTTP;
 using cloud.charging.open.protocols.OCPPv1_6.CP;
 using org.GraphDefined.Vanaheimr.Hermod.DNS;
+using org.GraphDefined.Vanaheimr.Hermod;
 
 namespace OCPP_ChargingStation
 {
@@ -16,7 +17,7 @@ namespace OCPP_ChargingStation
             var dnsClient  = new DNSClient();
 
             var cs01       = new OCPPv1_6.TestChargePointNode(
-                                 ChargeBoxId:               NetworkingNode_Id.Parse("CS01"),
+                                 ChargeBoxId:               NetworkingNode_Id.Parse("test01"),
                                  Connectors:                [
                                                                 new OCPPv1_6.CP.ConnectorSpec(
                                                                     Availability:        OCPPv1_6.Availabilities.Operative,
@@ -39,14 +40,36 @@ namespace OCPP_ChargingStation
 
 
             var ws01          = await cs01.ConnectOCPPWebSocketClient(
-                                          RemoteURL:   URL.Parse("ws://ocpp-public-demo.topazev.app:8081/AchimFriedland44"),
-                                          DNSClient:   dnsClient
+                                          RemoteURL:                   URL.Parse("wss://c.electriqua.com/abesp7/test01"),//ws://ocpp-public-demo.topazev.app:8081/AchimFriedland44"),
+                                          RemoteCertificateValidator:  (sender, certificate, chain, client, policyErrors) => {
+                                                                           return TLSValidationResult.Success();
+                                                                       },
+                                          DNSClient:                   dnsClient
                                       );
             var ws01response  = ws01.HTTPStatusCode;
 
-            var request01     = await cs01.SendBootNotification(
-                                          "vendor1"
-                                      );
+
+            var sendBootNotification = true;
+
+            while (true)
+            {
+
+                await Task.Delay(1000);
+
+                if (sendBootNotification)
+                {
+
+                    var request01  = await cs01.SendBootNotification(
+                                         "vendor1"
+                                     );
+
+                    DebugX.Log("BootNotification response: " + request01.Status);
+
+                    sendBootNotification = false;
+
+                }
+
+            }
 
 
         }
